3e522d61a0d2bc2a27e6f84847dafb405da10e80
shipko <shipko@ez-shipko.ez-shipko>
2020-05-27 12:40:41 +0700

    updated setup.py and created write_workbook_into_excel_file method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='excel-worksheet-maker',
+    version='0.0.1',
+    description='Utils for fast forming xlswriter Workbook file using strict format column list and data list',
+    url='git@github.com:evgenyshipko/excel-worksheet-maker.git',
+    author='Shipko Evgeny',
+    author_email='evgeny-shipko@ya.ru',
+    license='unlicense',
+    packages=['excel-worksheet-maker'],
+    zip_safe=False, install_requires=['xlsxwriter']
+)
